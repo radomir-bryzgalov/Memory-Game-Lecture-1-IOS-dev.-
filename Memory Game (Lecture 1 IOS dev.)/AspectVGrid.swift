327628846b004b7832543784f964b9e08f27a1f8
radomir-bryzgalov <rb8745359@gmail.com>
2023-12-04 00:10:29 -0500

    Finished Lecture 6

---
+++ Memory Game (Lecture 1 IOS dev.)/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memory Game (Lecture 1 IOS dev.)
+//
+//  Created by Радомир Радомир on 12/3/23.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
