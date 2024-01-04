3f5415ed7f0ef3c8605a18961dd9f3b100d227d9
radomir-bryzgalov <rb8745359@gmail.com>
2024-01-04 14:31:00 -0500

    Lecture 8 complete (animations up to score)

---
+++ Memory Game (Lecture 1 IOS dev.)/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  Memory Game (Lecture 1 IOS dev.)
+//
+//  Created by Радомир Радомир on 1/4/24.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
